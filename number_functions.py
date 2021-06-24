da23bcae3cfc6ace96c17403259bd46cf571fb7f
Giancarlo <70995660+Gian0206@users.noreply.github.com>
2021-06-24 14:06:01 +0100

    Update number_functions.py

--- number_functions.py
+++ number_functions.py
@@ -1,13 +1,18 @@
 def dif_even_odd(values_1: list, values_2: list) -> tuple:
     """
-    Sum two list of the same range item by item and returns a tuple
-    containing in the even index (i = 0) how many even sums where
-    made, and in the odd index (i = 1) how many odds where made;
+    Sum two list of the same range item by item and
+    returns a list containing a tuple that in his
+    even index (i = 0) shows how many even sums where made,
+    and in the odd index (i = 1) how many odds where made.
+    
+    The last value of the list (an integer) shows
+    the module (positive value) of the difference
+    between the two values obtanied inside the tuple;
     
     For e.g:
     # input:  A = [3, 5, 4, 3, 6]
     #         B = [2, 3, 5, 8, 2] 
-    # output: C = (2, 3)
+    # output: C = [(2, 3), 1]
     """
     evens = 0
     odds = 0
@@ -17,5 +22,14 @@ def dif_even_odd(values_1: list, values_2: list) -> tuple:
             evens += 1
         else:
             odds += 1
-    output = (evens, odds)
+    
+    if evens >= odds:
+        dif = evens - odds
+    else:
+        dif = odds - evens
+    
+    pair = (evens, odds)
+    output = []
+    output.append(pair)
+    output.append(dif)
     return output
